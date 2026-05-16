a50c18b412f435f858e4254580f2ab67007b2ae2
David <davidkrawiec@posteo.net>
2026-05-16 13:25:20 +0200

    extend update.sh

--- stage2a/prerun.sh
+++ stage2a/prerun.sh
@@ -1,3 +1,25 @@
 #!/bin/bash -e
 
-curl https://raw.githubusercontent.com/daviel/pigo-config/main/update.sh | sh
+if [ ! -d "${ROOTFS_DIR}" ]; then
+	copy_previous
+fi
+
+on_chroot << 'EOF'
+CONFIG_DIR="/opt/pigo/pigo-config"
+
+apt-get update
+apt-get install -y git device-tree-compiler
+
+mkdir -p /opt/pigo
+
+if [ ! -d "$CONFIG_DIR" ]; then
+	git clone "https://github.com/daviel/pigo-config" "$CONFIG_DIR"
+else
+	cd "$CONFIG_DIR"
+	git reset --hard
+	git pull
+fi
+
+cd "$CONFIG_DIR"
+grep -v '^sudo su' install.sh | bash -s
+EOF
