ca61995c4c7880112aaa6002f65704fbff1aa5ad
daviel <davidkrawiec@posteo.net>
2026-05-16 09:21:57 +0200

    Merge master to arm64 (#1)
    * add stage2a for pigo configuration
    
    * skip stage 3,4,5
    
    * git status
    
    * download install script raw and execute
    
    * dont upgrade packages
    
    * dont export stage4 and stage5
    
    * dont create image on stage2
    
    * Update 00-run-chroot.sh
    
    * install buster release of userconf-pi
    
    * Update 00-packages
    
    * Update 00-packages
    
    * remove rpi-swap
    
    * Aktualisieren von 00-packages
    
    * Aktualisieren von 00-packages-nr
    
    * Aktualisieren von 00-packages
    
    * move pigo config to export-image
    
    * dont install 64 bit kernel
    
    * export on stage2
    
    * make run chroot
    
    * move pigo stage
    
    * Add linux-image-rpi-v8 to firmware packages
    
    * Add wireless-tools to package list
    
    * Update base image from 'bookworm' to 'trixie'
    
    ---------
    
    Co-authored-by: daviel <david@Air-von-David.lan>

--- export-image/02-set-sources/01-run.sh
+++ export-image/02-set-sources/01-run.sh
@@ -4,7 +4,5 @@ rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 rm -f "${ROOTFS_DIR}/etc/apt/sources.list.d/00-temp.list"
 find "${ROOTFS_DIR}/var/lib/apt/lists/" -type f -delete
 on_chroot << EOF
-apt-get update
-apt-get -y dist-upgrade --auto-remove --purge
 apt-get clean
 EOF
